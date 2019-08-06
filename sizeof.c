67ead2aeb1d1b37703fd42c784beedbb02827174
abechoi <abraham.choi@icloud.com>
2019-08-05 20:23:11 -0700

    sizeof() function

---
+++ sizeof.c
@@ -0,0 +1,14 @@
+/*
+	sizeOf() - returns the size of a string.
+	%ul - unsigned longs argument.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	char string[] = "Hello World";
+	printf("%lu\n", sizeof(string));
+
+	return 0;
+}
